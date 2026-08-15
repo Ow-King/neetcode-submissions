ac4d8e6d2e4e7265388ae2d1c58789fec59d675a
Owen Kington <142945596+Ow-King@users.noreply.github.com>
2026-08-14 20:04:34 -0500

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        dp = {}
+
+        def dfs(i, j, k):
+            if k == len(s3): # If we are at the end of the string make sure that we have gone through both of the other arrays
+                return(i == len(s1) and j == len(s2))
+
+            if (i,j) in dp:
+                return dp[(i, j)]
+
+            res = False
+            if i < len(s1) and s1[i] == s3[k]:
+                res = dfs(i + 1, j, k + 1)
+            if not res and j < len(s2) and s2[j] == s3[k]:
+                res = dfs(i, j + 1, k + 1)
+            
+            dp[(i, j)] = res
+
+            return res
+
+        return dfs(0, 0, 0)
+
+
+            
