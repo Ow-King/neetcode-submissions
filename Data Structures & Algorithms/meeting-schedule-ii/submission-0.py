758132562c2576ac6468d4c99f4de0bff5f93a4f
Owen Kington <142945596+Ow-King@users.noreply.github.com>
2026-08-14 22:25:16 -0500

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key = lambda i: i.start)
+        rooms = []
+
+        for i in intervals:
+            if rooms and rooms[0] <= i.start:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms, i.end)
+
+        return len(rooms)
